eb5f033852bfb4dbc4647aa25ce42dff7c978243
Wilson dos Santos Alberto <will.neto@outlook.com>
2024-10-20 23:36:12 -0300

    Rename

---
+++ src/DuelCards.FanGame.WebGame.API/Program.cs
@@ -0,0 +1,29 @@
+namespace DuelCards.FanGame.WebGame.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+
+
+
+    }
+}
